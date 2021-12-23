24a2b39612deb3f0d36496a86c6f035a3332edb6
Aziz Al Azfar Rahee <76874466+azizalazfar@users.noreply.github.com>
2021-12-23 10:45:14 +0600

    Counting DNA Nucleotides.py

--- Python/DNA.py
+++ Python/DNA.py
@@ -1,4 +1,4 @@
 with open('rosalind_rna.txt') as f:
 	dataset = f.read()
 
-print(dataset.count('A'), dataset.count('C'), dataset.count('G'), dataset.count('T'), sep = ' ')
+print(dataset.count('A'), dataset.count('C'), dataset.count('G'), dataset.count('T'), sep = ' ') 
